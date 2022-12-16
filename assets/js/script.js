d05b6e82944e68fe256406602e242790d1b4c353
Mauritz Majelyne <mauritz.majelyne@hotmail.com>
2022-12-15 20:35:16 -0500

    js update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var marvelUrl = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b4cf87a8867f352c532cbf6b1548a717&hash=0c0886ca5bcf5b7a6ab7cf772bc6995a&limit=100&offset=`;
+offsetValue = 0
+
+for  (var i=0; i <81; i++) {
+  $.ajax({
+  url: marvelUrl + offsetValue,
+  method: 'GET',
+  }).then(function (response) {
+    console.log(response);
+  });
+  offsetValue = offsetValue + 100
+}
