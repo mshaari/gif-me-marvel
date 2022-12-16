90cdb474ed33c54b98cedaca311a841029a73913
fycn <fannychen623@gmail.com>
2022-12-15 21:05:49 -0500

    loaded giphy api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,28 @@
-var marvelUrl = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b4cf87a8867f352c532cbf6b1548a717&hash=0c0886ca5bcf5b7a6ab7cf772bc6995a&limit=100&offset=`;
-offsetValue = 0
+// var marvelUrl = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b4cf87a8867f352c532cbf6b1548a717&hash=0c0886ca5bcf5b7a6ab7cf772bc6995a&limit=100&offset=`;
+// offsetValue = 0
 
-for  (var i=0; i <81; i++) {
-  $.ajax({
-  url: marvelUrl + offsetValue,
-  method: 'GET',
-  }).then(function (response) {
-    console.log(response);
-  });
-  offsetValue = offsetValue + 100
-}
+// for  (var i=0; i <81; i++) {
+//   $.ajax({
+//   url: marvelUrl + offsetValue,
+//   method: 'GET',
+//   }).then(function (response) {
+//     console.log(response);
+//   });
+//   offsetValue = offsetValue + 100
+// }
 
 $('#searchGifBtn').on('click', function () {
-    var giphyUrl = `https://api.giphy.com/v1/gifs/search?q=${term}&apikey=pYOXbtrExM145EIidmGN2JB89VuG71Pj`;
     var term = $('input').val();
-
+    var giphyUrl = `https://api.giphy.com/v1/gifs/search?q=${term}&apikey=pYOXbtrExM145EIidmGN2JB89VuG71Pj`;
+    
     $.ajax({
     url: giphyUrl,
     method: 'GET',
     }).then(function (response) {
         console.log(response);
+        for (var i=0; i<10; i++) {
+            url = response.data[i].source
+            $(`<img src='${url}'>`).appendTo('.gifs');
+        };
     });
 });
