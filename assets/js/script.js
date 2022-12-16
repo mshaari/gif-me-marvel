b967c714476c835c4de1c7b960d114b96d2f52f8
Mauritz Majelyne <mauritz.majelyne@hotmail.com>
2022-12-15 20:44:50 -0500

    added Giphy js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var marvelUrl = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b4cf87a8867f352c532cbf6b1548a717&hash=0c0886ca5bcf5b7a6ab7cf772bc6995a&limit=100&offset=`;
 offsetValue = 0
 
-for  (var i=0; i <81; i++) {
+/*for  (var i=0; i <81; i++) {
   $.ajax({
   url: marvelUrl + offsetValue,
   method: 'GET',
@@ -10,10 +10,11 @@ for  (var i=0; i <81; i++) {
   });
   offsetValue = offsetValue + 100
 }
-
+*/
 $('#searchGifBtn').on('click', function () {
-    var giphyUrl = `https://api.giphy.com/v1/gifs/search?q=${term}&apikey=pYOXbtrExM145EIidmGN2JB89VuG71Pj`;
     var term = $('input').val();
+    var giphyUrl = `https://api.giphy.com/v1/gifs/search?q=${term}&apikey=pYOXbtrExM145EIidmGN2JB89VuG71Pj`;
+    console.log(giphyUrl);
 
     $.ajax({
     url: giphyUrl,
